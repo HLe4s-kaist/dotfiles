cbd93c797fa555ea33f5088423de2b1ded309b77
HLe4s-kaist <hle4s@kaist.ac.kr>
2026-01-19 18:19:10 +0900

    added undo history configure

--- nvim/lua/config/setting.lua
+++ nvim/lua/config/setting.lua
@@ -4,3 +4,6 @@ vim.cmd("set nocursorcolumn")
 vim.cmd("set cc=80")
 vim.cmd("set mouse=")
 vim.cmd([[colorscheme teide-dark]])
+-- enabling and setting for nvim-lualine
+require('lualine').setup()
+vim.opt.undofile = true
