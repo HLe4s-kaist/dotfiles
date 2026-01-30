df8ff633052732669796e473a4d26d3d71d160e1
HLe4s-kaist <hle4s@kaist.ac.kr>
2026-01-30 16:24:20 +0900

    added pylsp install script

--- nvim/install-lsp-bash.sh
+++ nvim/install-lsp-bash.sh
@@ -3,11 +3,11 @@
 echo "Installing LSPs.."
 case $ID in
   ubuntu)
-	sudo apt update && sudo apt install -y clangd; sudo apt install -y lua-language-server
+	sudo apt update && sudo apt install -y clangd; sudo apt install -y lua-language-server; sudo apt install python3-pylsp
     ;;
 
   arch) 
-	yes | sudo pacman -Syu && yes | sudo pacman -S clangd lua-language-server
+	yes | sudo pacman -Syu && yes | sudo pacman -S clangd lua-language-server python-lsp-server
     ;;
 
   *) echo "Your distribution is not supported :("
